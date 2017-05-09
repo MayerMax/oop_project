b82bdc9bad823b013f21927090dbd477ec39b17b
MayerMax <endurancemayer@yandex.ru>
2017-05-09 22:03:53 +0500

    Added class for better Line manipulation

---
+++ oopProject/Game/Position.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopProject.Game
+{
+    class Position
+    {
+        public FootballCard Card { get; set; }
+        public int Number { get; private set; }
+        public bool IsFree { get { return Card == null; } }
+        public Position(int place, FootballCard card) {
+            Card = card;
+            Number = place;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var card = (FootballCard)obj;
+            if (card.cardName.Equals(Card.cardName))
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public void ReleasePosition() {
+            Card = null;
+        }
+    }
+}
