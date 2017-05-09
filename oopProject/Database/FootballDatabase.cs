f7b6960782c3d68af8ef4daf17213bec59ed9094
Elena Arslanova <contilen@gmail.com>
2017-05-09 13:57:23 +0500

    FootballDatabase added

---
+++ oopProject/Database/FootballDatabase.cs
@@ -0,0 +1,35 @@
+﻿using oopProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopProject
+{
+    class FootballDatabase
+    {
+        private IDatabase database;
+        private Dictionary<ZoneType, string[]> types = new Dictionary<ZoneType, string[]>
+        {
+            [ZoneType.GK] = new[] { "GK" },
+            //TODO
+        };
+
+        public FootballDatabase(IDatabase database)
+        {
+            this.database = database;
+        }
+
+        public FootballCard GetCardOfType(ZoneType zone)
+        {
+            return new FootballCard((FootballPlayerInfo)database.GetPlayerOfType(types[zone]), ZoneType.NONE);
+        }
+
+        public IEnumerable<FootballCard> GetCards(int count)
+        {
+            foreach (var playerInfo in database.GetPlayers(count))
+                yield return new FootballCard((FootballPlayerInfo)playerInfo, ZoneType.NONE);
+        }
+    }
+}
