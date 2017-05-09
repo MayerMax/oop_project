158f912ff09337794e8b8190d4200379959be9e1
MayerMax <endurancemayer@yandex.ru>
2017-05-09 15:04:53 +0500

    added types for football database

--- oopProject/Database/FootballDatabase.cs
+++ oopProject/Database/FootballDatabase.cs
@@ -13,23 +13,40 @@ namespace oopProject
         private Dictionary<ZoneType, string[]> types = new Dictionary<ZoneType, string[]>
         {
             [ZoneType.GK] = new[] { "GK" },
-            //TODO
+            [ZoneType.DEF] = new[] {"CB", "LCB", "RCB", "LB", "RB", "Sub" },
+            [ZoneType.MID] = new[] {"CM", "LDM", "RDM", "CDM", "CAM", "LM", "RM" },
+            [ZoneType.ATT] = new[] { "ST", "CF", "LW", "RW"}
         };
+        private Dictionary<string, ZoneType> reversedTypes;
+
 
         public FootballDatabase(IDatabase database)
         {
             this.database = database;
+            reversedTypes = CompleteRotation();
         }
 
         public FootballCard GetCardOfType(ZoneType zone)
         {
-            return new FootballCard((FootballPlayerInfo)database.GetPlayerOfType(types[zone]), ZoneType.NONE);
+            return new FootballCard((FootballPlayerInfo)database.GetPlayerOfType(types[zone]), zone);
         }
 
         public IEnumerable<FootballCard> GetCards(int count)
         {
             foreach (var playerInfo in database.GetPlayers(count))
-                yield return new FootballCard((FootballPlayerInfo)playerInfo, ZoneType.NONE);
+            {
+                var correctZone = reversedTypes[playerInfo["Club_Position"]];
+                yield return new FootballCard((FootballPlayerInfo)playerInfo, correctZone);
+            }
+        }
+
+        private Dictionary<string, ZoneType> CompleteRotation() {
+            var reversedDictionary = new Dictionary<string, ZoneType>();
+            foreach (var record in types) {
+                foreach (var position in record.Value)
+                    reversedDictionary[position] = record.Key;
+            }
+            return reversedDictionary;
         }
     }
 }
