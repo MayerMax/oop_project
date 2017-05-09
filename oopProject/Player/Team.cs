a2fec3db0edffd3f6e86a3dc135d6a9f59003215
MayerMax <endurancemayer@yandex.ru>
2017-05-09 22:02:30 +0500

    Wrapper around Player

---
+++ oopProject/Player/Team.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopProject
+{
+    class Team
+    {
+        public readonly Squad Squad;
+        public readonly Hand Hand;
+
+        public Team(Squad starts, Hand subs) {
+            Squad = starts;
+            Hand = subs;
+        }
+
+        public void SubstitutionFromHandToSquad(ZoneType type, FootballCard oldCard, FootballCard newCard) {
+            Hand.Remove(newCard);
+            Squad.Remove(type, oldCard);
+            Squad.Insert(type, newCard);
+            Hand.InsertCard(oldCard);
+        }
+
+    }
+}
