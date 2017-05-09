0cc42065392bd335f6b11a2fef71f20dafa87880
MayerMax <endurancemayer@yandex.ru>
2017-05-09 23:15:36 +0500

    Actions are started

---
+++ oopProject/Actions/Substitute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopProject
+{
+    class SwapCard : IAction
+    {
+        private Team team;
+        public SwapCard(Team team) {
+            this.team = team;
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Explanation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAvailable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
