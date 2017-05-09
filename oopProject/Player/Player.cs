b180c51cb24cd214beeddf2996c976507bd046f7
MayerMax <endurancemayer@yandex.ru>
2017-05-09 21:59:31 +0500

    Player class

--- oopProject/Player/Player.cs
+++ oopProject/Player/Player.cs
@@ -8,5 +8,33 @@ namespace oopProject
 {
     class Player
     {
+        public readonly string Name;
+        public readonly string Formation;
+
+        private Squad sqad;
+        private Hand hand;
+        public Team team;
+
+        public Player(FootballDatabase db, string name, string formation) {
+            if (!Squad.ValidateSquad(formation))
+                throw new ArgumentException("Incorrect squad given!");
+            Name = name;
+            Formation = formation;
+            var filledSquad = FillSquadWithRandom(formation, db);
+            hand = new Hand(new List<FootballCard> { }); // Left it free for now
+            sqad = new Squad(name, db.GetCardOfType(ZoneType.GK), filledSquad);
+            team = new Team(sqad, hand);
+        }
+
+        private Dictionary<ZoneType, List<FootballCard>> FillSquadWithRandom(string formation, FootballDatabase db) {
+            int[] eachzoneSize = formation.Split('-').Select(elem => int.Parse(elem)).ToArray();
+
+            var resultFiller = new Dictionary<ZoneType, List<FootballCard>>();
+            resultFiller.Add(ZoneType.DEF, db.GetCards(eachzoneSize[0]).ToList());
+            resultFiller.Add(ZoneType.MID, db.GetCards(eachzoneSize[1]).ToList());
+            resultFiller.Add(ZoneType.ATT, db.GetCards(eachzoneSize[2]).ToList());
+
+           return resultFiller;
+        }
     }
 }
