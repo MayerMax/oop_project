d857522350712aa3239f8414ebee1d5164325e45
MayerMax <endurancemayer@yandex.ru>
2017-05-09 20:05:52 +0500

    Deck class

---
+++ oopProject/Game/Deck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopProject.Game
+{
+    class Deck
+    {
+        public static readonly int DECK_SIZE = 50;
+
+        private Stack<FootballCard> deck;
+
+        public Deck(FootballDatabase db) {
+            deck = new Stack<FootballCard>( db.GetCards(DECK_SIZE));
+        }
+
+        public FootballCard GetCard() {
+            return deck.Pop();
+        }
+
+
+    }
+}
