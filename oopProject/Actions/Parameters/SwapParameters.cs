f1e2ff66ceb52366b318accd1d6423626a1e176e
Elena Arslanova <contilen@gmail.com>
2017-05-27 20:29:59 +0500

    Parameters for IAction

---
+++ oopProject/Actions/Parameters/SwapParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopProject
+{
+    class SwapParameters : IParameters
+    {
+        public readonly int OldCardPosition;
+        public readonly int NewCardPosition;
+        public readonly FootballCard NewCard;
+
+        public SwapParameters(int oldCardPosition, FootballCard newCard, int newCardPosition)
+        {
+            OldCardPosition = oldCardPosition;
+            NewCardPosition = newCardPosition;
+            NewCard = newCard;
+        }
+    }
+}
