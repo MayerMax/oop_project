0c15177818911f8d3a67d3bfdf0129621556c6d1
MayerMax <endurancemayer@yandex.ru>
2017-05-31 03:19:02 +0500

    extra class as Visitor

---
+++ oopProject/Success.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopProject
+{
+     interface ISuccess {
+
+         string Message { get;}
+         void Apply(SwapAction action, bool isTrue);
+         void Apply(PassAction action, bool isTrue);
+        //TODO
+
+    }
+
+    class Success : ISuccess
+    {
+        public string Message
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            private set { }
+        }
+
+        public void Apply(PassAction action, bool isTrue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Apply(SwapAction action, bool isTrue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
