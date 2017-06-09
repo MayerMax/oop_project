e68db2ecc7441c4fbd8c595f32e856b44a2761f8
Elena Arslanova <contilen@gmail.com>
2017-06-10 02:29:45 +0500

    solution

--- oopProject/Parsers/ConsoleParsers/ConsoleParser.cs
+++ oopProject/Parsers/ConsoleParsers/ConsoleParser.cs
@@ -66,7 +66,7 @@ namespace oopProject
 
         protected IEnumerable<ZoneType> VerifyZoneTypes(string[] parameters, int[] indexes)
         {
-            if (parameters.Length < indexes.Length)
+            if (parameters.Length <= indexes.Last())
                 throw new ArgumentException("Invalid parameters");
             return parameters.Select(VerifyZoneType);
         }
